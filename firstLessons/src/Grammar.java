918a2a2ae68352c766c8f9f5d42473a233541462
Nargiz-Aliyeva <nargizaliyeva0120@gmail.com>
2026-08-21 22:33:12 +0400

    Son dəyişikliklər

--- firstLessons/src/Grammar.java
+++ firstLessons/src/Grammar.java
@@ -3,16 +3,32 @@ public class Grammar {
     // mes sekil yuklemek instagramda 3 yerde minimum sekil yuklemek  mumkundur  status post profilfront end 3 kod yazir ve gunun sonunda
     // o 3 kod  1 backend kodununu isledir biz kodu ele yaziriqki 1 backend kodu islesin
     //class
+    // public sözü classın visibility sini gösterir yeni başqa mertebe bu mertebedeki classı görsün ya görmesin.
+    // public sözü acces modifire dir
+
     // acces modifire
+
     // class adlari böyüklə yazılır eger 1 adda birden cox soz varsa her bir söz böyük hərflə yazılıtr MyFirstClass
-    // Metod - Metod yalniz class daxilinde yazila biler
+
+    // Metod - Metod yalniz class daxilinde yazila biler (otaq)
     //Javada metod adı camelCase yazılır yeni  ilk söz ilk herf balaca
     //camelCase - metod adinda
     // PascalCase - Class adinda
     // snake_case -
     // data type -
     // 1primitive data type  - kicik herfle yazilir - int , float, double , char , boolean
+    // double floatin daha boyuk variantidir
+    // char simvollar
+    //
     // 2 none primitive data type (wrapper) - boyukle yazilir  - class , String
+    // butun primitiv tiplerin boyuk herfle yazilan versiyasi var ve biz olara class deyirik
+    // Javada soz cumle yazmaq ucun primiyiv data type yoxdur ancaq wrapper/
     // comment
+    //int i =2;
+  //  i+=6 //i=i+6
+
+//i++ ;i = i+1
+//    i--=i-1
+   // i*=5 = i*5
 
 }
--- firstLessons/src/Main.java
+++ firstLessons/src/Main.java
@@ -1,6 +1,46 @@
+import javax.swing.*;
+import javax.swing.plaf.PanelUI;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+public static void main(String[]args){
+   // var username = "jet001";
 
+    // 1 primitive data types int float , double, char
+    // 2 none primitiv (wrapper) böyük
+    int a =2;
+    int b = 3;
+    int c = a+b;
+    //System.out.println(c);
+    //boolean d = c>0;
+  //  System.out.println(c>0);
+int y = 23;
+boolean t =true;
+  /*  Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter first number:");
+    int s = scanner.nextInt();
+    System.out.print("Enter second number");
+    int r = scanner.nextInt();
+    System.out.println("Result:");
+    System.out.println(s*r);*/
+String username = "agent 007";
+
+    System.out.println("welcome, "+ username);
+    System.out.println("welcome, "+ username+3+5);
+    System.out.println("welcome, "+ username+(3+5));
+    int i =2;
+    i+=6;
+    System.out.println(i);
+Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+
+String name = scanner.next();
+System.out.println(" Enter your age: ");
+int age = scanner.nextInt();
+    System.out.println(name+" "+ (age>18));
+
+}
 
 }
