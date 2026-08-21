53ddd06027501b977c3e75cc97c45d7f6b8cdae7
Nargiz-Aliyeva <nargizaliyeva0120@gmail.com>
2026-08-21 17:08:48 +0400

    Son dəyişikliklər

--- firstLessons/src/Grammar.java
+++ firstLessons/src/Grammar.java
@@ -1,3 +1,18 @@
 public class Grammar {
+    // object oriented programming 1 1ine problem yaratmayan kodlar yazmaq ve 1 yerde yaz 1 nece yerde  defelerle istifade et
+    // mes sekil yuklemek instagramda 3 yerde minimum sekil yuklemek  mumkundur  status post profilfront end 3 kod yazir ve gunun sonunda
+    // o 3 kod  1 backend kodununu isledir biz kodu ele yaziriqki 1 backend kodu islesin
+    //class
+    // acces modifire
+    // class adlari böyüklə yazılır eger 1 adda birden cox soz varsa her bir söz böyük hərflə yazılıtr MyFirstClass
+    // Metod - Metod yalniz class daxilinde yazila biler
+    //Javada metod adı camelCase yazılır yeni  ilk söz ilk herf balaca
+    //camelCase - metod adinda
+    // PascalCase - Class adinda
+    // snake_case -
+    // data type -
+    // 1primitive data type  - kicik herfle yazilir - int , float, double , char , boolean
+    // 2 none primitive data type (wrapper) - boyukle yazilir  - class , String
+    // comment
 
 }
