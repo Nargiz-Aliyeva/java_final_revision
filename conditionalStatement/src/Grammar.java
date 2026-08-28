59c6526bc4457a68431d2e3bf726d15bff95442f
Nargiz-Aliyeva <nargizaliyeva0120@gmail.com>
2026-08-29 00:15:26 +0400

    Son dəyişikliklər

--- conditionalStatement/src/Grammar.java
+++ conditionalStatement/src/Grammar.java
@@ -41,5 +41,20 @@ else {
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-// Looplar :
+// githubdan qeyd
+    /*
+git status
+git add .
+git commit -m "Son dəyişikliklər"
+git push
+
+git push origin main
+
+git push
+
+son deyisikliler elave olunsun deye
+
+
+     */
 }
+
