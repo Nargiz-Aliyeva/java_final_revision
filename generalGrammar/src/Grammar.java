f1b30061402e376b148c4b11d9c285609b1a47ab
Nargiz-Aliyeva <nargizaliyeva0120@gmail.com>
2026-09-25 10:19:33 +0400

    Son dəyişikliklər

---
+++ generalGrammar/src/Grammar.java
@@ -0,0 +1,18 @@
+public class Grammar {
+    // githubdan qeyd
+    /*
+git status
+git add .
+git commit -m "Son dəyişikliklər"
+git push
+
+git push origin main
+
+git push
+
+son deyisikliler elave olunsun deye
+
+
+     */
+
+}
